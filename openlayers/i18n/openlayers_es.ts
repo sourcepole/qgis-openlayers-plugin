4c4f8c501645eef2d05779400d6d79578375106d
Matías <taborda.barroso@gmail.com>
2015-09-30 20:35:32 +0200

    Update openlayers_es.ts

--- openlayers/i18n/openlayers_es.ts
+++ openlayers/i18n/openlayers_es.ts
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1" language="en">
+<!DOCTYPE TS><TS version="1.1" language="es">
 <context>
     <name>Form</name>
     <message>
@@ -10,32 +10,32 @@
     <message>
         <location filename="ui_openlayers_ovwidget.py" line="73"/>
         <source>Enable map</source>
-        <translation>Enable map</translation>
+        <translation>Activa mapa</translation>
     </message>
     <message>
         <location filename="ui_openlayers_ovwidget.py" line="74"/>
         <source>Add map</source>
-        <translation>Add map</translation>
+        <translation>Añadir mapa</translation>
     </message>
     <message>
         <location filename="ui_openlayers_ovwidget.py" line="75"/>
         <source>Hide cross in map</source>
-        <translation>Hide cross in map</translation>
+        <translation>Ocultar cruz en mapa</translation>
     </message>
     <message>
         <location filename="ui_openlayers_ovwidget.py" line="76"/>
         <source>Refresh map</source>
-        <translation>Refresh map</translation>
+        <translation>Actualizar mapa</translation>
     </message>
     <message>
         <location filename="ui_openlayers_ovwidget.py" line="77"/>
         <source>Save this image</source>
-        <translation>Save this image</translation>
+        <translation>Salvar esta imagen</translation>
     </message>
     <message>
         <location filename="ui_openlayers_ovwidget.py" line="78"/>
         <source>Copy rectangle (KML) of map to clipboard</source>
-        <translation>Copy rectangle (KML) of map to clipboard</translation>
+        <translation>Copiar rectángulo (KML) del mapa al portapapeles</translation>
     </message>
 </context>
 <context>
@@ -43,32 +43,32 @@
     <message>
         <location filename="openlayers_ovwidget.py" line="235"/>
         <source>Save image</source>
-        <translation>Save image</translation>
+        <translation>Salvar imagen</translation>
     </message>
     <message>
         <location filename="openlayers_ovwidget.py" line="235"/>
         <source>Image(*.jpg)</source>
-        <translation>Image(*.jpg)</translation>
+        <translation>Imagen(*.jpg)</translation>
     </message>
     <message>
         <location filename="openlayers_ovwidget.py" line="248"/>
         <source>OpenLayers Overview</source>
-        <translation>OpenLayers Overview</translation>
+        <translation>Menú OpenLayers</translation>
     </message>
     <message>
         <location filename="openlayers_ovwidget.py" line="158"/>
         <source>At least one layer in map canvas required</source>
-        <translation>At least one layer in map canvas required</translation>
+        <translation>Se requiere al menos una capa en el escritorio</translation>
     </message>
     <message>
         <location filename="openlayers_ovwidget.py" line="248"/>
         <source>Error loading page!</source>
-        <translation>Error loading page!</translation>
+        <translation>¡Error leyendo página!</translation>
     </message>
     <message>
         <location filename="openlayers_ovwidget.py" line="258"/>
         <source>Loading %1...</source>
-        <translation>Loading %1...</translation>
+        <translation>Leyendo %1...</translation>
     </message>
 </context>
 <context>
@@ -76,17 +76,17 @@
     <message>
         <location filename="openlayers_plugin.py" line="144"/>
         <source>OpenLayers Overview</source>
-        <translation>OpenLayers Overview</translation>
+        <translation>Menú OpenLayers</translation>
     </message>
     <message>
         <location filename="openlayers_plugin.py" line="154"/>
         <source>Add %1 layer</source>
-        <translation>Add %1 layer</translation>
+        <translation>Añadir %1 capa</translation>
     </message>
     <message>
         <location filename="openlayers_plugin.py" line="161"/>
         <source>Could not set Google projection!</source>
-        <translation>Could not set Google projection!</translation>
+        <translation>¡Imposible establecer proyección de Google!</translation>
     </message>
 </context>
 </TS>
